fbdab55b14e88c4a3c9e580fbfd6d36b4f0e9ff5
Michael Kroon <kroonology@gmail.com>
2019-09-12 00:25:59 -0600

    add playerCard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
+const PlayerCard = ({color, symbol}) => {
+  const style = { backgroundColor: color }
+  return (
+    <div style={ style } className='player-card'>{ symbol }</div>
+  )
+}
+
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.symbols = ['paper', 'rock', 'scissors'];
+  }
+
+  startGame = () => {
+    const index = Math.floor(Math.random()*3);
+    console.log(this.symbols[index]);
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <PlayerCard color='red' symbol='paper'/>
+        <PlayerCard color='blue' symbol='rock'/>
+        <button onClick={this.startGame}>Run Game</button>
+      </div>
+    )
+  }
 }
 
 export default App;
