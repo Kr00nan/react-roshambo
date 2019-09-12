6c9182624c655f93d2801461a35763688f7560a7
Michael Kroon <kroonology@gmail.com>
2019-09-12 02:54:59 -0600

    final submit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
 import React from 'react';
+import './App.css';
 
-const PlayerCard = ({color, symbol}) => {
+const PlayerChoice = ({color, choice}) => {
   const style = { backgroundColor: color,
-                  backgroundImage: 'url(./img/' + symbol + '.png)' }
+                  backgroundImage: 'url(./img/' + choice + '.png)' };
   return (
-    <div style={ style } className='player-card'>{ symbol }</div>
+    <div style={ style } className='player-icon'></div>
   )
 }
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.symbols = ['paper', 'rock', 'scissors'];
+    this.choices = ['paper', 'rock', 'scissors'];
     this.state = {};
   }
 
+  decideWinner = () => {
+    const { playerOne, playerTwo } = this.state;
+    if ( playerOne === playerTwo ) {
+      return 'It\'s a draw!';
+    }
+    if ((playerOne === 'rock' && playerTwo === 'scissors') ||
+        (playerOne === 'paper' && playerTwo === 'rock') ||
+        (playerOne === 'scissors' && playerTwo === 'paper')) {
+          return 'Player One is the winner!';
+        }
+    return 'Player Two is the winner!';
+  }
+
   startGame = () => {
-    this.setState({
-      playerRed: this.symbols[Math.floor(Math.random()*3)],
-      playerBlue: this.symbols[Math.floor(Math.random()*3)]
-    });
+    let counter = 0;
+    let myInterval = setInterval(() => {
+      counter++;
+      this.setState({
+        playerOne: this.choices[Math.floor(Math.random()*3)],
+        playerTwo: this.choices[Math.floor(Math.random()*3)],
+        winner: ''
+      })
+      if (counter > 10) {
+        clearInterval(myInterval);
+        this.setState({ winner: this.decideWinner });
+      }
+    }, 100);
   }
 
   render() {
     return (
-      <div className='App'>
-        <PlayerCard color='red' symbol={this.state.playerRed}/>
-        <PlayerCard color='blue' symbol={this.state.playerBlue}/>
-        <button onClick={this.startGame}>Run Game</button>
+      <div>
+        <PlayerChoice color='firebrick' choice={ this.state.playerOne }/>
+        <PlayerChoice color='cadetblue' choice={ this.state.playerTwo }/>
+        <p>{ this.state.winner }</p>
+        <button onClick={ this.startGame }>Run Game</button>
       </div>
-    )
+    );
   }
 }
 
